aea3aa89311f2fdbbffd89c14fdf413a60dbbf91
Thomas O Fredericks <thomas@fredericks.info>
2025-06-09 13:22:50 -0400

    MàJ

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,10 @@ void begin() {
 }
 
 void step() {
-  // UNCOMMENT ONE OF THE FOLLOWING FOR THE INDICATED EFFECT
+ // UNCOMMENT ONE OF THE FOLLOWING FOR THE INDICATED EFFECT
  // pqWave.phase(otherWave);  // BACK AND FORTH MOVEMENT
  // pqWave.phase(0); // FIXED
 
  pqWave.frequency(2); // THE NUMBER OF TIMES IT REPEATS IN ONE SECOND
- pqStrip.applyWave(pqWave, 1.0);  // Float value = THE TIME DURACTION TO DISPLAY
+ pqStrip.applyWave(pqWave, 1.0);  // Float value = THE TIME DURATION TO DISPLAY
 }
