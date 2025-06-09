c8701e25ac056e71c4a529c8ba5ca77f12904e21
Thomas O Fredericks <thomas@fredericks.info>
2025-06-09 13:07:21 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+
+
+
+#include <Plaquette.h>
+#include <FastLED.h>
+
+#include <PqLEDStrip.h>
+PqStripWS281X<4,RGB,16> pqStrip;  // PIN RGB_ORDER LENGTH
+
+
+SineWave pqWave{2.0};
+SineWave otherWave{2.0};
+
+void begin() {
+
+}
+
+void step() {
+ pqWave.phase(otherWave);
+ pqWave.frequency(2);
+ pqStrip.applyWave(pqWave, 1.0);  
+}
