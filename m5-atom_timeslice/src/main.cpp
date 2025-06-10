d9955833528b141289ffb87390dd00961e68d86d
Thomas O Fredericks <thomas@fredericks.info>
2025-06-10 10:52:11 -0400

    Pseudocode

---
+++ m5-atom_timeslice/src/main.cpp
@@ -0,0 +1,35 @@
+
+THIS EXAMPLE IS PSEUDOCODE AND DOES NOT COMPILE
+
+
+#include <Plaquette.h>
+#include <FastLED.h>
+
+#include <PqLEDStrip.h>
+PqStripWS281X<4, RGB, 16> pqStrip; // PIN RGB_ORDER LENGTH
+
+TimeSlice timeSlive{0.05, 16}; // Period of 50ms and 16 samples (one sample per pixel)
+
+SineWave sineWave{2.0};
+
+
+void begin()
+{
+}
+
+void step()
+{
+
+   sineWave.period(0.05); // 50ms period
+    
+    sineWave >> timeSlice; // TIMESLICE WILL SAMPLE TO FILL 16 SAMPLES OVER 1 SECOND
+
+    if ( timeSlice.full() ) {
+        pqStrip.apply(timeSlice); // draw the TimeSlice to the strip
+        pqWave.phase(0); // sync the wave to the TimeSlice
+    }
+
+    if ( pqStrip.full() ) {
+        pqWave.phase(0); // sync the wave to the strip
+    }
+}
